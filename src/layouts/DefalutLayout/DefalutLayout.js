27d4b4846c3ebe98822e4c9efc7890301910290e
Willmind <tanzhijingege@126.com>
2021-01-05 11:39:31 +0800

    1

---
+++ src/layouts/DefalutLayout/DefalutLayout.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class DefalutLayout extends Component {
+    render() {
+        return (<div id="DefalutLayout" >HelloWorld
+        <Button type="primary">123</Button>
+        </div>)
+    }
+
+}
